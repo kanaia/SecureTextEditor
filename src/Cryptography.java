2dadfc168a42a0d22ae024da4c9f4ef5c27845b6
Marvin Schöning <schoening.marvin@web.de>
2017-06-06 17:14:23 +0200

    fix padding

--- src/Cryptography.java
+++ src/Cryptography.java
@@ -1,3 +1,4 @@
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
@@ -8,7 +9,7 @@ public class Cryptography {
 
 	public String encryptAES(String s, String cipherMode, String padding) throws Exception {
 		String ciphermethod = "AES/" + cipherMode + "/" + padding;
-		byte[] input = Base64.getDecoder().decode(s);
+		byte[] input = s.getBytes(StandardCharsets.US_ASCII);
 		byte[] keyBytes = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c,
 				0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
 
@@ -54,7 +55,7 @@ public class Cryptography {
 		int ctLength = cipher.update(input, 0, input.length, plainText, 0);
 		ctLength += cipher.doFinal(plainText, ctLength);
 
-		return Base64.getEncoder().encodeToString(plainText);
+		return new String(plainText, StandardCharsets.US_ASCII);
 	}
 
 	public String encryptDES(String s, String cipherMode, String padding) throws Exception {
